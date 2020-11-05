f931d1e3297baf2149b57f65f2e5922470a6b1ec
Rendy Ananta <rendyananta66@gmail.com>
2020-11-05 22:42:54 +0700

    add method create attachment from existing file in storage

--- src/Concerns/AttachmentCreator.php
+++ src/Concerns/AttachmentCreator.php
@@ -6,6 +6,7 @@ namespace Jalameta\Attachments\Concerns;
 
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Jalameta\Attachments\Entities\Attachment;
 
@@ -19,15 +20,17 @@ trait AttachmentCreator
      * @param array $attributes
      * @return Attachment
      */
-    public function create(UploadedFile $file, array $attributes = [])
+    public function create(UploadedFile $file, array $attributes)
     {
         $attachment = new Attachment();
 
-        $attributes = Arr::add($attributes, 'path', $this->store($file));
+        $disk = Arr::get($attributes, 'disk');
+
+        $attributes = Arr::add($attributes, 'path', $this->store($file, $disk));
         $attributes = Arr::add($attributes, 'mime', $file->getMimeType());
 
         $attachment->fill(
-            Arr::only($attributes, ['title', 'mime', 'type', 'options', 'description'])
+            Arr::only($attributes, ['title', 'mime', 'type', 'path', 'disk', 'options', 'description'])
         );
 
         $attachment->save();
@@ -39,13 +42,42 @@ trait AttachmentCreator
      * Store file into disk
      *
      * @param UploadedFile $file
+     * @param null $disk
      * @return false|string
      */
-    public function store(UploadedFile $file)
+    public function store(UploadedFile $file, $disk = null)
     {
         return $file->storeAs('/', Str::uuid(), [
-            'disk' => config('attachment.disk')
+            'disk' => $disk ?? config('attachment.disk')
         ]);
+    }
+
+    /**
+     * Create attachment from file that have been stored in filesystems.
+     *
+     * @param string $path
+     * @param array $attributes
+     * @param null $disk
+     * @return Attachment
+     */
+    public function createFromPath(string $path, array $attributes, $disk = null)
+    {
+        $attachment = new Attachment();
+
+        $attributes = Arr::add($attributes, 'disk', $disk ?? config('attachment.disk'));
+        $attributes = Arr::add($attributes, 'path', $path);
+
+        $file = Storage::disk(Arr::get($attributes, 'disk'))->mimeType(Arr::get($attributes, 'path'));
+
+        $attributes = Arr::add($attributes, 'mime', $file);
+
+        $attachment->fill(
+            Arr::only($attributes, ['title', 'mime', 'type', 'path', 'disk', 'options', 'description'])
+        );
+
+        $attachment->save();
+
+        return $attachment;
     }
 
 }
